9c834f7fde651cda93ac270bcae28eac652bc62a
Bruno Mello <bgorresenm@gmail.com>
2021-09-26 11:35:11 -0500

    fix: correção ao pegar preço do carbono

--- helper.py
+++ helper.py
@@ -1,11 +1,12 @@
 from functools import lru_cache
+from datetime import datetime
 
 import pandas as pd
 
 
-def get_carbon_price(date: str) -> float:
+def get_carbon_price(date: datetime) -> float:
     df = pd.read_csv("carbon_price.csv")
-    return df.set_index("year").loc[date.year, "price"]
+    return df.set_index("year").loc[date.year].iloc[0]
 
 
 @lru_cache()
